6c63e5d2a96bb1396e9988cae076668a5fc87e16
mycopi <30847515+mycopi@users.noreply.github.com>
2017-08-08 18:16:26 -0500

    without more text

--- mycophp.php
+++ mycophp.php
@@ -3,7 +3,7 @@
 <body>
 <h1>Welcome to my home page!</h1>
 <p>Some text.</p>
-<p>Some more text.</p>
+
 <?php include 'db_login.php';?>
 <?php echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>"; ?>
 </body>
