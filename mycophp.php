94a343effe7c63332d25472a04cee0953ad4fc6d
mycopi <30847515+mycopi@users.noreply.github.com>
2017-08-08 18:13:10 -0500

    initial commit

---
+++ mycophp.php
@@ -0,0 +1,10 @@
+<html>
+<header><title>This is title</title></header>
+<body>
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+<?php include 'db_login.php';?>
+<?php echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>"; ?>
+</body>
+</html>
