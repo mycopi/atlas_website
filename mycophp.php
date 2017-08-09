e5c6909b8cb3283907d361fa18bb45f757f2e4b2
mycopi <30847515+mycopi@users.noreply.github.com>
2017-08-08 20:01:38 -0500

    php updates
    issuing request and populating results- probably not working but still cool

--- mycophp.php
+++ mycophp.php
@@ -4,7 +4,17 @@
 <h1>Welcome to my home page!</h1>
 <p>Some text.</p>
 
-<?php include 'db_login.php';?>
+	<?php include 'db_login.php';?>
+	<?php $request = http_request(GET, "http://192.168.1.108/list_address");
+	echo $request;?>
+<ol>
+
+	<?php for ($i=0, $i<= $request->body, $i++) {
+	 echo "<li>" . $request->body[$i] . "</li>";
+	}
+	?>
+
+</ol>
 <?php echo "<p>Copyright &copy; 1999-" . date("Y") . " W3Schools.com</p>"; ?>
 </body>
 </html>
